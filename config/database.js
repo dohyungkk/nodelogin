6a9029c29c43ffdf3440eb594eb76009fbeba237
Dohyung Kim <kdkim7676@gmail.com>
2018-11-22 02:22:23 -0800

    database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
  'connection':{
-  'host':'localhost',
+  'host':'mysql://bf8b2a417287c7:072596f5@us-cdbr-iron-east-01.cleardb.net/heroku_6f687bb36813ea6?reconnect=true',
   'user':'root',
   'password':'13243546'
  },
