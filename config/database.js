510eb8777ac5b843e29df68f6d3248eebeb7f30e
Dohyung Kim <kdkim7676@gmail.com>
2018-11-22 14:04:39 -0800

    database added

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 module.exports = {
  'connection':{
-  'host':'mysql://bf8b2a417287c7:072596f5@us-cdbr-iron-east-01.cleardb.net/heroku_6f687bb36813ea6?reconnect=true',
-  'user':'root',
-  'password':'13243546'
+  // 'host':'localhost',
+  // 'user':'root',
+  // 'password':'13243546'
+  'host':'us-cdbr-iron-east-01.cleardb.net',
+  'user':'bf8b2a417287c7',
+  'password':'072596f5'
  },
- 'database':'registration',
- 'user_table':'users'
+ // 'database':'registration',
+ // 'user_table':'users'
+ 'database':'heroku_6f687bb36813ea6'
 }
