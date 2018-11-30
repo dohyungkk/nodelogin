2357271520d4c59d187084a61d4a9a7b882b04a1
Dohyung Kim <kdkim7676@gmail.com>
2018-11-30 15:09:29 -0800

    database for heroku

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
+// module.exports = {
+//  'connection':{
+//   'host':'localhost',
+//   'user':'root',
+//   'password':'13243546'
+//  },
+//  'database':'registration',
+//  'user_table':'users'
+// }
+
 module.exports = {
  'connection':{
-  // 'host':'localhost',
-  // 'user':'root',
-  // 'password':'13243546'
   'host':'us-cdbr-iron-east-01.cleardb.net',
   'user':'bf8b2a417287c7',
   'password':'072596f5'
  },
- // 'database':'registration',
- // 'user_table':'users'
  'database':'heroku_6f687bb36813ea6'
- // 'user_table':'users'
+ 'user_table':'users'
 }
